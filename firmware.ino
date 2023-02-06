38409e4767668d2b3f45a55a4668d6ccf888be88
Lucas Leite <me@lucasleite.us>
2023-02-05 21:35:55 -0500

    Update motors positions

--- firmware.ino
+++ firmware.ino
@@ -10,13 +10,13 @@
 void run_demo()
 {
     move_forward();
-    delay(1000);
+    delay(5000);
     move_backward();
-    delay(1000);
+    delay(5000);
     move_left();
-    delay(1000);
+    delay(5000);
     move_right();
-    delay(1000);
+    delay(5000);
     stop();
 }
 
@@ -35,52 +35,85 @@ void setup()
     run_demo();
 }
 
-void move_forward()
+void m1_forward()
 {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
+}
+
+void m2_forward()
+{
+
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    digitalWrite(IN5, HIGH);
-    digitalWrite(IN6, LOW);
+}
+
+void m3_forward()
+{
+    digitalWrite(IN5, LOW);
+    digitalWrite(IN6, HIGH);
+}
+
+void m4_forward()
+{
     digitalWrite(IN7, HIGH);
     digitalWrite(IN8, LOW);
+}
+void m1_backward()
+{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+}
+
+void m2_backward()
+{
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+}
+
+void m3_backward()
+{
+    digitalWrite(IN5, LOW);
+    digitalWrite(IN6, HIGH);
+}
+
+void m4_backward()
+{
+    digitalWrite(IN7, HIGH);
+    digitalWrite(IN8, LOW);
+}
+
+void move_forward()
+{
+    m1_forward();
+    m2_forward();
+    m3_forward();
+    m4_forward();
 }
 
 void move_backward()
 {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-    digitalWrite(IN5, LOW);
-    digitalWrite(IN6, HIGH);
-    digitalWrite(IN7, LOW);
-    digitalWrite(IN8, HIGH);
+    m1_backward();
+    m2_backward();
+    m3_backward();
+    m4_backward();
 }
 
 void move_right()
 {
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-    digitalWrite(IN5, HIGH);
-    digitalWrite(IN6, LOW);
-    digitalWrite(IN7, LOW);
-    digitalWrite(IN8, HIGH);
+    m1_forward();
+    m2_forward();
+    m3_backward();
+    m4_backward();
 }
 
 void move_left()
 {
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    digitalWrite(IN5, LOW);
-    digitalWrite(IN6, HIGH);
-    digitalWrite(IN7, HIGH);
-    digitalWrite(IN8, LOW);
+    m1_backward();
+    m2_backward();
+    m3_forward();
+    m4_forward();
 }
 
 void stop()
